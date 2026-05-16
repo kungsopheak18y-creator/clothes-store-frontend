008b1207bdb20cee114a195c9e44ee29a43e6336
Sopheak Kung <kungsopheak18y@gmail.com>
2026-05-16 17:49:53 +0700

    hardcode railway url

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8000',
+  baseURL: 'https://clothes-store-backend.up.railway.app',
   headers: {
     'Accept': 'application/json',
     'Content-Type': 'application/json',
