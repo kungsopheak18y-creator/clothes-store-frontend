bb51408b9bf0b3f31009a64299a6a9ab38e85e6e
Sopheak Kung <kungsopheak18y@gmail.com>
2026-05-18 15:43:38 +0700

    fix back

--- src/store/authStore.js
+++ src/store/authStore.js
@@ -1,22 +1,22 @@
 import { create } from 'zustand'
 
 const useAuthStore = create((set) => ({
-  user: null,
+  user:      null,
   isLoading: true,
 
   setUser: (userData, token) => {
-    if (token) localStorage.setItem('token', token);
+  if (token) localStorage.setItem('token', token);
 
-    // Normalize snake_case → camelCase once here
-    const user = {
-      ...userData,
-      firstName: userData.first_name ?? userData.firstName ?? '',
-      lastName: userData.last_name ?? userData.lastName ?? '',
-      dateOfBirth: userData.date_of_birth ?? userData.dateOfBirth ?? '',
-    };
+  // Normalize snake_case → camelCase once here
+  const user = {
+    ...userData,
+    firstName: userData.first_name ?? userData.firstName ?? '',
+    lastName:  userData.last_name  ?? userData.lastName  ?? '',
+    dateOfBirth: userData.date_of_birth ?? userData.dateOfBirth ?? '',
+  };
 
-    set({ user, isLoading: false });
-  },
+  set({ user, isLoading: false });
+},
 }))
 
 export default useAuthStore
